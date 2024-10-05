feede823af86e0cba0927ce722fc9af65b0ebabc
Edgar Coime <e.coimeii@hotmail.com>
2024-10-04 17:31:44 -0700

    feat: nonels and cmp installed

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,58 +1,52 @@
 -- Set leader key to space
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 local keymap = vim.keymap
 local opts = function(desc)
-	return { noremap = true, silent = true, desc = desc }
+  return { noremap = true, silent = true, desc = desc }
 end
 
 -- cutting single characters don't put to register
-keymap.set("n", "x", '"_x"')
+keymap.set('n', 'x', '"_x"')
 
 -- Diagnostic keymaps
-keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts("Open diagnostic [Q]uickfix list"))
-
+keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts 'Open diagnostic [Q]uickfix list')
 
 -- Set ctrl q and ctrl s as save
-keymap.set("n", "<C-s>", "<cmd>w!<cr>", opts("Force Write"))
-keymap.set("n", "<C-q>", "<cmd>qa!<cr>", opts("Force Quit"))
+keymap.set('n', '<C-s>', '<cmd>w!<cr>', opts 'Force Write')
+keymap.set('n', '<C-q>', '<cmd>qa!<cr>', opts 'Force Quit')
 
 -- TODO: Not working ?
 -- keymap.set("n", "<leader>q", "<cmd>q!<CR>", { desc = "Close current buffer" }) --  move current buffer to new tab
 
 -- editor configs
 -- Toggle line wrap with <leader>w
-vim.api.nvim_set_keymap(
-	"n",
-	"<leader>el",
-	"<cmd>set wrap!<CR>",
-	opts("Toggle line wrap")
-)
+vim.api.nvim_set_keymap('n', '<leader>el', '<cmd>set wrap!<CR>', opts 'Toggle line wrap')
 
 -- tabs
-keymap.set("n", "<leader>te", "<cmd>tabedit<cr>", opts("Edit tab"))
-keymap.set("n", "<tab>", "<cmd>tabnext<Return><cr>", opts("Progress forward tabs"))
-keymap.set("n", "<s-tab>", "<cmd>tabprev<Return><cr>", opts("Progress backward tabs"))
-keymap.set("n", "<leader>tq", "<cmd>tabclose<cr>", opts("Close tab"))
+keymap.set('n', '<leader>te', '<cmd>tabedit<cr>', opts 'Edit tab')
+keymap.set('n', '<tab>', '<cmd>tabnext<Return><cr>', opts 'Progress forward tabs')
+keymap.set('n', '<s-tab>', '<cmd>tabprev<Return><cr>', opts 'Progress backward tabs')
+keymap.set('n', '<leader>tq', '<cmd>tabclose<cr>', opts 'Close tab')
 
 -- window management
-keymap.set("n", "\\", "<C-w>s", opts("Split window horizontally"))
-keymap.set("n", "|", "<C-w>v", opts("Split window vertically"))
+keymap.set('n', '\\', '<C-w>s', opts 'Split window horizontally')
+keymap.set('n', '|', '<C-w>v', opts 'Split window vertically')
 
 -- Move window
-keymap.set("n", "<leader>th", "<C-w>h", opts("Move tab left"))
-keymap.set("n", "<leader>tl", "<C-w>l", opts("Move tab right"))
-keymap.set("n", "<leader>tj", "<C-w>j", opts("Move tab down"))
-keymap.set("n", "<leader>tk", "<C-w>k", opts("Move tab up"))
+keymap.set('n', '<leader>th', '<C-w>h', opts 'Move tab left')
+keymap.set('n', '<leader>tl', '<C-w>l', opts 'Move tab right')
+keymap.set('n', '<leader>tj', '<C-w>j', opts 'Move tab down')
+keymap.set('n', '<leader>tk', '<C-w>k', opts 'Move tab up')
 
 -- resize window
-keymap.set("n", "<C-S-h>", "<C-w><", opts("Resize left"))
-keymap.set("n", "<C-S-l>", "<C-w>>", opts("Resize right"))
-keymap.set("n", "<C-S-j>", "<C-w>-", opts("Resize down"))
-keymap.set("n", "<C-S-k>", "<C-w>+", opts("Resize up"))
+keymap.set('n', '<C-S-h>', '<C-w><', opts 'Resize left')
+keymap.set('n', '<C-S-l>', '<C-w>>', opts 'Resize right')
+keymap.set('n', '<C-S-j>', '<C-w>-', opts 'Resize down')
+keymap.set('n', '<C-S-k>', '<C-w>+', opts 'Resize up')
 
 -- diagnostics
-keymap.set("n", "<C-j>", function()
-	vim.diagnostic.goto_next()
-end, opts("Go to next diagnostic"))
+keymap.set('n', '<C-j>', function()
+  vim.diagnostic.goto_next()
+end, opts 'Go to next diagnostic')
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,118 +1,128 @@
 return {
-	{
-		"nvimdev/dashboard-nvim",
-		enabled = false,
-	},
-	{
-		"nvim-lualine/lualine.nvim",
-		enabled = false,
-	},
-	-- messages, cmdline and the popupmenu
-	
- {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- add any options here
+  {
+    'nvimdev/dashboard-nvim',
+    enabled = false,
   },
-  dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
-    "rcarriga/nvim-notify",
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+      {
+        'ThePrimeagen/harpoon',
+        branch = 'harpoon2',
+        requires = { { 'nvim-lua/plenary.nvim' } },
+      },
+      'nvimtools/none-ls.nvim', -- configure formatters & linters
+    },
+    event = 'VeryLazy',
+    config = function()
+      require 'custom.lualine'
+    end,
   },
 
-  config = function()
-    local keymap = vim.keymap
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      'rcarriga/nvim-notify',
+    },
 
-			local focused = true
-			vim.api.nvim_create_autocmd("FocusGained", {
-				callback = function()
-					focused = true
-				end,
-			})
-			vim.api.nvim_create_autocmd("FocusLost", {
-				callback = function()
-					focused = false
-				end,
-			})
+    config = function()
+      local keymap = vim.keymap
 
-    require("noice").setup({
-      lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+      local focused = true
+      vim.api.nvim_create_autocmd('FocusGained', {
+        callback = function()
+          focused = true
+        end,
+      })
+      vim.api.nvim_create_autocmd('FocusLost', {
+        callback = function()
+          focused = false
+        end,
+      })
+
+      require('noice').setup {
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+            ['vim.lsp.util.stylize_markdown'] = true,
+            ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+          },
         },
-      },
-      -- you can enable a preset for easier configuration
-      presets = {
-        bottom_search = true, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = true, -- add a border to hover docs and signature help
-      },
-    })
-  end,
-},
+        -- you can enable a preset for easier configuration
+        presets = {
+          bottom_search = true, -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = true, -- add a border to hover docs and signature help
+        },
+      }
+    end,
+  },
 
-	{
-		"rcarriga/nvim-notify",
-		opts = {
-			timeout = 2000,
-			render = "wrapped-compact",
-		},
-	},
+  {
+    'rcarriga/nvim-notify',
+    opts = {
+      timeout = 2000,
+      render = 'wrapped-compact',
+    },
+  },
 
-	-- -- buffer line
-	-- {
-	-- 	"akinsho/bufferline.nvim",
-	-- 	event = "VeryLazy",
-	-- 	keys = {
-	-- 		{ "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
-	-- 		{ "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
-	-- 	},
-	-- 	opts = {
-	-- 		options = {
-	-- 			mode = "tabs",
-	-- 			show_buffer_close_icons = false,
-	-- 			show_close_icon = false,
-	-- 		},
-	-- 	},
-	-- },
+  -- -- buffer line
+  -- {
+  -- 	"akinsho/bufferline.nvim",
+  -- 	event = "VeryLazy",
+  -- 	keys = {
+  -- 		{ "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+  -- 		{ "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+  -- 	},
+  -- 	opts = {
+  -- 		options = {
+  -- 			mode = "tabs",
+  -- 			show_buffer_close_icons = false,
+  -- 			show_close_icon = false,
+  -- 		},
+  -- 	},
+  -- },
 
-	-- filename
-	{
-		"b0o/incline.nvim",
-		dependencies = {},
-		event = "BufReadPre",
-		priority = 1200,
-		config = function()
-			local helpers = require("incline.helpers")
-			require("incline").setup({
-				window = {
-					padding = 0,
-					margin = { horizontal = 0 },
-				},
-				render = function(props)
-					local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-					local ft_icon, ft_color = require("nvim-web-devicons").get_icon_color(filename)
-					local modified = vim.bo[props.buf].modified
-					local buffer = {
-						ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) }
-							or "",
-						" ",
-						{ filename, gui = modified and "bold,italic" or "bold" },
-						" ",
-						guibg = "#363944",
-					}
-					return buffer
-				end,
-			})
-		end,
-	},
+  -- filename
+  {
+    'b0o/incline.nvim',
+    dependencies = {},
+    event = 'BufReadPre',
+    priority = 1200,
+    config = function()
+      local helpers = require 'incline.helpers'
+      require('incline').setup {
+        window = {
+          padding = 0,
+          margin = { horizontal = 0 },
+        },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
+          local ft_icon, ft_color = require('nvim-web-devicons').get_icon_color(filename)
+          local modified = vim.bo[props.buf].modified
+          local buffer = {
+            ft_icon and { ' ', ft_icon, ' ', guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or '',
+            ' ',
+            { filename, gui = modified and 'bold,italic' or 'bold' },
+            ' ',
+            guibg = '#363944',
+          }
+          return buffer
+        end,
+      }
+    end,
+  },
 }
