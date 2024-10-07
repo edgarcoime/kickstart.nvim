6982701263e7e7ff192719c528f4e91ae01cab22
Edgar Coime <e.coimeii@hotmail.com>
2024-10-07 01:16:13 -0700

    feat: add auto tags

--- lua/plugins/lsp/lsp.lua
+++ lua/plugins/lsp/lsp.lua
@@ -31,7 +31,17 @@ return {
       { "antosha417/nvim-lsp-file-operations", config = true },
       "williamboman/mason-lspconfig.nvim",
       "WhoIsSethDaniel/mason-tool-installer.nvim",
+
+      -- Additionals
       "mrcjkb/rustaceanvim",
+      {
+        "windwp/nvim-ts-autotag",
+        opts = {
+          enable_close = true, -- Auto close tags
+          enable_rename = true, -- Auto rename pairs of tags
+          enable_close_on_slash = false, -- Auto close on trailing </
+        },
+      },
     },
     config = function()
       -- import lspconfig plugin
